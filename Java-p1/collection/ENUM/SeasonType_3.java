e111d3dd32110a004fd70c97c8c6c597b4f98d48
Yeonjae-Cho <72658768+Yeonjae-Cho@users.noreply.github.com>
2021-02-10 23:55:26 +0900

    enum practice

---
+++ Java-p1/collection/ENUM/SeasonType_3.java
@@ -0,0 +1,5 @@
+package my.day18.a.ENUM;
+//Enum Type : enum(열거형)이라는 타입은 "서로 연관된 상수들의 집합"을 의미하는 것이다. 
+public enum SeasonType_3 {
+	봄, 여름, 가을, 겨울		// 겨울; 와 같이 끝에 ; 을 붙이지 않는다.
+}
---
+++ Java-p1/collection/ENUM/Season_1.java
@@ -0,0 +1,10 @@
+package my.day18.a.ENUM;
+
+public class Season_1 {
+
+	public static final String SPRING="봄";
+	public static final String SUMMER="여름";
+	public static final String AUTUMN="가을";
+	public static final String WINTER="겨울";
+	
+}
